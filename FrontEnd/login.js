21e9e5bb5a35307af8c115c46877cc5dcffe29b6
Théo Rouillard <theo.rouillard@outlook.fr>
2024-05-24 15:50:54 +0200

    Modification login

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,34 +1,42 @@
-const apiUrl = 'http://localhost:5678/api/users/login';
+function login() {
+    const apiUrl = 'http://localhost:5678/api/users/login';
 
-// Informations d'identification
-const credentials = {
-    email: 'votre_email@example.com',
-    password: 'votre_mot_de_passe'
-};
+    // Récupérer les valeurs des champs de formulaire
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
 
-// Effectuer une requête POST pour obtenir le token
-fetch(apiUrl, {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(credentials)
-})
-    .then(response => {
-        // Vérifier si la réponse est OK (200)
-        if (!response.ok) {
-            throw new Error('Erreur lors de la connexion');
-        }
-        // Rediriger vers la page "accueil.html" après une connexion réussie
-        window.location.href = 'accueil.html';
+    // Informations d'identification
+    const credentials = {
+        email: email,
+        password: password
+    };
+
+    // Effectuer une requête POST pour obtenir le token
+    fetch(apiUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(credentials)
     })
-    .catch(error => {
-        // Gérer les erreurs lors de la connexion
-        console.error('Erreur:', error);
-    });
-
-
-
+        .then(response => {
+            // Vérifier si la réponse est OK (200)
+            if (!response.ok) {
+                throw new Error('Erreur lors de la connexion');
+            }
+            return response.json(); // Convertir la réponse en JSON
+        })
+        .then(data => {
+            // Sauvegarder le token dans le local storage
+            saveTokenToLocalStorage(data.token);
+            // Rediriger vers la page "accueil.html" après une connexion réussie
+            window.location.href = 'accueil.html';
+        })
+        .catch(error => {
+            // Gérer les erreurs lors de la connexion
+            console.error('Erreur:', error);
+        });
+}
 
 // Fonction pour sauvegarder le token dans le local storage
 function saveTokenToLocalStorage(token) {
@@ -40,10 +48,3 @@ function getTokenFromLocalStorage() {
     return localStorage.getItem('token');
 }
 
-// Sauvegarder le token
-const token = 'votre_token';
-saveTokenToLocalStorage(token);
-
-// Récupérer le token
-const retrievedToken = getTokenFromLocalStorage();
-console.log(retrievedToken); // Affichera le token récupéré depuis le local storage
